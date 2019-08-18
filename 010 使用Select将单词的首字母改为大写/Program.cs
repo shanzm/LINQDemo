cfe5a16bfbc8e1b4b07132bc6612017091edc8ca
shanzm <2351144690@qq.com>
2019-08-19 01:01:36 +0800

    2019年8月19日 01:01:07-update 使用扩展方法

--- 010 使用Select将单词的首字母改为大写/Program.cs
+++ 010 使用Select将单词的首字母改为大写/Program.cs
@@ -11,14 +11,38 @@ namespace _010_使用Select将单词的首字母改为大写
     {
         static void Main(string[] args)
         {
+            #region 使用Linq表达式
+            //string str = "i am shanzm";
+            //string[] strArray = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //var query = from o in strArray
+            //            select o.Replace(o[0], Convert.ToChar(o[0].ToString().ToUpper()));
+            //Array.ForEach(query.ToArray(), o => Console.Write(o + " "));
+            //Console.ReadKey();
+            #endregion
+
+
+            #region 使用Linq函数式
             string str = "i am  shanzm";
             string[] strArray = Regex.Split(str, @"\s+").ToArray();
             var query = strArray.Select(o => o.Replace(o[0], Convert.ToChar(o[0].ToString().ToUpper())));
 
-            Array.ForEach(query.ToArray(), o => Console.Write(o + " "));
+            //Array.ForEach(query.ToArray(), o => Console.Write(o + " "));
+            //我们可以把打印数组的功能使用扩展方法的形式，添加到IEnumerable<T>类中
+            query.WriteLineAll();
             Console.ReadKey();
-
+            #endregion
 
         }
+
     }
+    #region 关于使用this关键字实现扩展方法
+    public static class Extender
+    {
+        public static void WriteLineAll<T>(this IEnumerable<T> array)
+        {
+            Array.ForEach(array.ToArray(), o => Console.Write(o + " "));
+        }
+    }
+    #endregion
 }
