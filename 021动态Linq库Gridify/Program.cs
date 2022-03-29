b6d0923ddd13f4f7da4dc6eb6eb063d15f63b97e
shanzm_aimer <2351144690@qq.com>
2022-03-29 13:25:25 +0800

    2022年3月29日 13:25:05-Add:动态LInq生成，使用Gridify库

---
+++ 021动态Linq库Gridify/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _021动态Linq库Gridify
+{
+    class Program
+    {
+        //https://jishuin.proginn.com/p/763bfbd73082
+        static void Main(string[] args)
+        {
+        }
+    }
+}
