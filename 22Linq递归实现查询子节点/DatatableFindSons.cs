bc1e9e4b93e3ebaed9bb4383a61612bc028294de
shanzm_aimer <2351144690@qq.com>
2022-10-27 11:37:25 +0800

    2022年10月27日 11:37:13-Update：树形结构查询父子节点

---
+++ 22Linq递归实现查询子节点/DatatableFindSons.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22Linq递归实现查询子节点
+{
+    public static class DatatableFindSons
+    {
+        public static void Demo()
+        {
+            DataTable dtRegion = GetDtRegion();
+
+            DataSet ds = new DataSet();
+            ds.Tables.Add(dtRegion);
+
+            //DataSet的Relations属性主要是用于建立主子表关系，这里我们将一张树形结构的表建立自连接
+            ds.Relations.Add("TreeRelation", ds.Tables[0].Columns["Id"], ds.Tables[0].Columns["PId"], false);
+
+            //获取指定的节点的所有下一级子节点（注意根据我们的关联关系，我们只能查找到一个节点的所有二级节点）
+            DataRow[] drSons = dtRegion.Select("Name='中国'")[0].GetChildRows("TreeRelation");
+            DataRow[] drParents = dtRegion.Select("Name='越溪街道'")[0].GetParentRows("TreeRelation");
+
+            //使用递归获取江苏省节点下的所有子节点
+            List<DataRow> listDr = GetSons(dtRegion.Select("Name='江苏省'")[0]);
+            List<DataRow> listDra = GetParents(dtRegion.Select("Name='越溪街道'")[0]);
+
+            listDr.ForEach(n => Console.WriteLine($"Id:{n["Id"]},PId:{n["PId"]},Name:{n["Name"]}"));
+            Console.WriteLine("---------------");
+            listDra.ForEach(n => Console.WriteLine($"Id:{n["Id"]},PId:{n["PId"]},Name:{n["Name"]}"));
+        }
+
+        /// <summary>
+        /// 广度遍历：获取指定的父节点的所有层级的子节点
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <returns></returns>
+        public static List<DataRow> GetSons(DataRow dr)
+        {
+            List<DataRow> drSons = dr.GetChildRows("TreeRelation").ToList();
+            List<DataRow> result = new List<DataRow>(drSons);
+            foreach (DataRow row in drSons)
+            {
+                result.AddRange(GetSons(row));
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 获取指定的节点的所有上级父节点
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <returns></returns>
+        public static List<DataRow> GetParents(DataRow dr)
+        {
+            List<DataRow> drParents = dr.GetParentRows("TreeRelation").ToList();
+            List<DataRow> result = new List<DataRow>(drParents);
+            foreach (DataRow row in drParents)
+            {
+                result.AddRange(GetParents(row));
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 创建测试数据
+        /// </summary>
+        /// <returns></returns>
+        private static DataTable GetDtRegion()
+        {
+            //建表
+            DataTable dtRegion = new DataTable("Region");
+            //建列
+            DataColumn dcId = new DataColumn("Id", typeof(int));
+            DataColumn dcPId = new DataColumn("PId", typeof(int));
+            DataColumn dcName = new DataColumn("Name", typeof(string));
+            DataColumn[] aryDc = { dcId, dcPId, dcName };
+            dtRegion.Columns.AddRange(aryDc);
+            //设置主键
+            //dcId.AllowDBNull = false;
+            //dtRegion.PrimaryKey = new DataColumn[] { dcId };
+            dtRegion.Rows.Add(new object[] { "1", "0", "中国" });
+            dtRegion.Rows.Add(new object[] { "2", "1", "江苏省" });
+            dtRegion.Rows.Add(new object[] { "3", "2", "苏州市" });
+            dtRegion.Rows.Add(new object[] { "4", "3", "吴中区" });
+            dtRegion.Rows.Add(new object[] { "5", "1", "山东省" });
+            dtRegion.Rows.Add(new object[] { "6", "5", "济南市" });
+            dtRegion.Rows.Add(new object[] { "7", "5", "青岛市" });
+            dtRegion.Rows.Add(new object[] { "8", "5", "烟台市" });
+            dtRegion.Rows.Add(new object[] { "9", "2", "南京市" });
+            dtRegion.Rows.Add(new object[] { "11", "9", "玄武区" });
+            dtRegion.Rows.Add(new object[] { "12", "4", "越溪街道" });
+            dtRegion.Rows.Add(new object[] { "13", "4", "横泾街道" });
+            return dtRegion;
+        }
+    }
+}
---
+++ 22Linq递归实现查询子节点/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22Linq递归实现查询子节点
+{
+    internal class Program
+    {
+        //参考：https://blog.csdn.net/smartsmile2012/article/details/81317327
+        private static void Main(string[] args)
+        {
+            //关于Datatable中的树形结构的建立关系并查询父子节点
+            //DatatableFindSons.Demo();
+
+            //使用Linq实现
+            List<Region> regions = GetSons(GetListRegion(), 5).ToList();
+            regions.ForEach(n => Console.WriteLine($"Id:{n.Id},PId:{n.PId},Name:{n.Name}"));
+            Console.ReadKey();
+        }
+
+        #region 获取所有下级
+
+        /// <summary>
+        /// 获取指定的节点其所有的子节点（包含指定节点本身）
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="pId"></param>
+        /// <returns></returns>
+        public static IEnumerable<Region> GetSonsWithSelf(IEnumerable<Region> list, int pId)
+        {
+            var query = list.Where(p => p.Id == pId).ToList();
+            return query.Concat(GetSons(list, pId));
+        }
+
+        /// <summary>
+        /// 获取指定的节点其所有的子节点
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="pId"></param>
+        /// <returns></returns>
+        public static IEnumerable<Region> GetSons(IEnumerable<Region> list, int pId)
+        {
+            var query = list.Where(p => p.PId == pId).ToList();
+            return query.ToList().Concat(query.ToList().SelectMany(t => GetSons(list, t.Id)));
+        }
+
+        #endregion
+
+        #region 获取所有上级
+
+        public static IEnumerable<Region> GetFatherList(IEnumerable<Region> list, int Id)
+        {
+            var query = list.Where(p => p.Id == Id).ToList();
+            return query.ToList().Concat(query.ToList().SelectMany(t => GetFatherList(list, t.PId)));
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 树形表实体对象
+        /// </summary>
+        public class Region
+        {
+            public int Id { get; set; }
+            public int PId { get; set; }
+            public string Name { get; set; }
+        }
+
+        /// <summary>
+        /// 模拟数据源
+        /// </summary>
+        /// <returns></returns>
+        public static List<Region> GetListRegion()
+        {
+            List<Region> listRegion = new List<Region>()
+            {
+                new Region (){Id=1, PId=0,Name="中国"    },
+                new Region (){Id=2, PId=1,Name="江苏省"  },
+                new Region (){Id=3, PId=2,Name="苏州市"  },
+                new Region (){Id=4, PId=3,Name="吴中区"  },
+                new Region (){Id=5, PId=1,Name="山东省"  },
+                new Region (){Id=6, PId=5,Name="济南市"  },
+                new Region (){Id=7, PId=5,Name="青岛市"  },
+                new Region (){Id=8, PId=5,Name="烟台市"  },
+                new Region (){Id=9, PId=2,Name="南京市"  },
+                new Region (){Id=11,PId=9,Name="玄武区"  },
+                new Region (){Id=12,PId=4,Name="越溪街道"},
+                new Region (){Id=13,PId=4,Name="横泾街道"}
+            };
+            return listRegion;
+        }
+    }
+}
