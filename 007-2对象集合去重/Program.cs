4a42085634442d7f2af032d4d16612dacf216226
shanzm_aimer <2351144690@qq.com>
2023-09-05 17:24:21 +0800

    2023年9月5日 17:24:13-Update

--- 007-2对象集合去重/Program.cs
+++ 007-2对象集合去重/Program.cs
@@ -1,24 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _007_2对象集合去重
 {
-    class Program
+    internal class Program
     {
         //参考：https://blog.csdn.net/laizhixue/article/details/89228355
+        //参考：https://www.cnblogs.com/CreateMyself/p/12863407.html
 
-
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             List<Student> students = GetListStu();
 
             //直接使用Distinct函数是无法对对象集合去重的
+            //因为为Distinct 默认比较的是对象的引用，即：字段值相同的两个对象，在内存中不同位置，则意味着是两个对象
             //IEnumerable<Student> result = students.Distinct();
             //result.ToList().ForEach(n => Console.WriteLine(n.ID + n.Name + n.Age + n.Gender));
-
 
             //法1
             //依旧是使用Distinct函数，其有两重重载，第二重就是使用自定义比较器
@@ -26,31 +24,27 @@ namespace _007_2对象集合去重
             //IEnumerable<Student> students1 = students.Distinct(new DistanctStudentComparer());
             //students1.ToList().ForEach(n => Console.WriteLine(n.ID + n.Name + n.Age + n.Gender));
 
-
             //法2
             //https://docs.microsoft.com/zh-cn/dotnet/api/system.linq.enumerable.distinct?view=net-5.0
             //Student实现IEquatable<T>接口,实现接口中的Equals和GetHashCode 函数
             //不好，项目中的对象一般轻易不要修改，为了实现比较两个对象，去对某个对象类实现该接口比较麻烦
+            //2023年9月5日 16:46:29
+            //不用实现接口，只是在对象类中对Equals和GetHashCode进行重写，见项目：【007-3对象集合去重】
 
-
-            //法3（推荐）
+            //法3（推荐——推荐的原始只是可以快速实现按照特定字段进行去重，其实这种方式性能最差）
             //使用group by聚合
             //List<Student> students2 = students.GroupBy(n => new { n.ID, n.Name, n.Age, n.Gender }).Select(n => n.First()).ToList();
             //students2.ForEach(n => Console.WriteLine(n.ID + n.Name + n.Age + n.Gender));
 
-
-            //法4
+            //法4（性能优）
             //使用扩展方法，对IEnumerable<TSource>扩展
             List<Student> students3 = students.DistinctBy(n => new { n.ID, n.Gender }).ToList();
             students3.ForEach(n => Console.WriteLine(n.ID + n.Name + n.Age + n.Gender));
 
-            
-
             Console.ReadKey();
         }
 
-
-        static List<Student> GetListStu()
+        private static List<Student> GetListStu()
         {
             List<Student> students = new List<Student>()
             {
@@ -63,9 +57,6 @@ namespace _007_2对象集合去重
             };
             return students;
         }
-
-
-
     }
 
     //自定义比较器
@@ -84,14 +75,30 @@ namespace _007_2对象集合去重
         }
     }
 
+    //使用HashSet去重
     //对IEnumerable<T>扩展一个DistinctBy方法
     public static class DistinctHelper
     {
         public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
         {
-            var identifiedKeys = new HashSet<TKey>();
+            //这里的去重逻辑很简单：就是定义一个HashSet，然后将数据添加到HashSet，从而获取到去重的数据
+            HashSet<TKey> identifiedKeys = new HashSet<TKey>();
             return source.Where(element => identifiedKeys.Add(keySelector(element)));
         }
+
+        //等同于上面的，只是上面是Where筛选直接返回集合，这里使用froeach和yield return
+        //public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,Func<TSource, TKey> keySelector)
+        //{
+        //    var hashSet = new HashSet<TKey>();
+
+        //    foreach (TSource element in source)
+        //    {
+        //        if (hashSet.Add(keySelector(element)))
+        //        {
+        //            yield return element;
+        //        }
+        //    }
+        //}
     }
 
     public class Student
---
+++ 23依据集合对另一个集合筛选/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _23依据集合对另一个集合筛选
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //需求很简单：有一个User对象集合，这个对象有一个Id字段，我们需要根据指定的Id集合筛选User集合
+            //可以理解为实现SQL中的IN筛选
+            //在Linq中可以使用Where配合Any实现该筛选
+
+            List<User> listUser = new List<User>
+            {
+                new User() { Id = 1, Age = 1, Name = "Name01" },
+                new User() { Id = 2, Age = 2, Name = "Name02" },
+                new User() { Id = 3, Age = 3, Name = "Name03" },
+                new User() { Id = 4, Age = 4, Name = "Name04" }
+            };
+
+            List<int> listId = new List<int>() { 1, 4, 5 };
+
+            //实现类似in筛选
+            List<User> result1 = listUser.Where(n => listId.Any(m => n.Id == m)).ToList();
+            //注意：这样是无法实现类似not in 的筛选，Any表示判读是否存在，存在在返回true
+            List<User> result2 = listUser.Where(n => listId.Any(m => n.Id != m)).ToList();
+            //todo:实现not in筛选
+            //List<User>  result3=listUser.Where(n=>listId.Contains)
+
+            result1.ForEach(n => Console.WriteLine(n.Id + n.Name + n.Age));
+            Console.WriteLine("------------");
+            result2.ForEach(n => Console.WriteLine(n.Id + n.Name + n.Age));
+
+            Console.ReadKey();
+        }
+    }
+
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
